35ab3b631afcf1af6ebbb6857cf71de0775717be
Carlos Sanchez <67176642+carlosmsanchezm@users.noreply.github.com>
2023-01-04 01:23:17 -0500

    Secrets removed

--- in_progress/es/elasticsearch_domain.tf
+++ in_progress/es/elasticsearch_domain.tf
@@ -7,11 +7,6 @@ terraform {
   }
 }
 
-provider "aws" {
-  region = "us-east-1"
-  access_key="AKIA3ULERJGBXS53NEHQ"
-  secret_key="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"
-}
 
 resource "aws_elasticsearch_domain" "tfer--gen2" {
   domain_name = "gen2"
