dc536d32837ab260e746bfc9b8c837af88cb2459
carlosmsanchezm <carlos.sanchez.m.m@gmail.com>
2023-01-10 09:25:24 -0500

    changing project layout, dependencies and variables

--- network/vpc/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "aws_vpc_tfer--vpc-072a71590b8c6a80c_id" {
-  value = "${aws_vpc.tfer--vpc-072a71590b8c6a80c.id}"
-}
--- network/vpc_peering/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "aws_vpc_peering_connection_tfer--pcx-0ca4afd59731e9bea_id" {
-  value = "${aws_vpc_peering_connection.market.id}"
-}
